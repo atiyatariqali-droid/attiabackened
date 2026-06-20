8df73c8196bfab7ea06fb7dec8b1e7fdf7aeedd9
unknown <atiyatariqali@gmail.com>
2026-06-20 09:50:51 -0700

    device binding

---
+++ database/migrations/2026_06_20_033823_add_session_id_to_attendance_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('attendance', function (Blueprint $table) {
+             $table->unsignedBigInteger('session_id')->nullable()->after('id');
+
+            $table->foreign('session_id')
+                  ->references('id')
+                  ->on('attendance_sessions')
+                  ->onDelete('set null');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('attendance', function (Blueprint $table) {
+            $table->dropForeign(['session_id']);
+            $table->dropColumn('session_id');
+        });
+    }
+};
